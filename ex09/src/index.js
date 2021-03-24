6db13e007b2ee663ac2ea07a9106c943cde62a7d
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-24 21:58:09 +0100

    ex09 Fina a user by ID

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,57 +1,68 @@
 //Lodash
-const _ = require('lodash');
+const _ = require("lodash");
 //only change code below this line
 //users nested array with four objects starts here
-const users = [
-    {id: 1,
-    firstName: John,
-    lastName: Doe,
-    age:24
-    gender:male},
+var users = [
+  { id: 1, firstName: "John", lastName: "Doe", age: 24, gender: "male" },
 
-    {id: 2,
-    firstName: Jane,
-    lastName: Doe,
-    age: 5,
-    gender: female},
+  { id: 2, firstName: "Jane", lastName: "Doe", age: 5, gender: "female" },
 
-    {id: 3,
-    firstName: Jim,
-    lastName: Carrey,
-    age: 54
-    gender: male},
+  { id: 3, firstName: "Jim", lastName: "Carrey", age: 54, gender: "male" },
 
-    {id: 4,
-    firstName: Kate
-    lastName: Winslet,
-    age: 40,
-    gender: female}
+  { id: 4, firstName: "Kate", lastName: "Winslet", age: 40, gender: "female" },
 ];
 //users nested array with four objects ends here
 //getUsers function - list of users starts here
-function getUsers(){
-    var output = "";
-    for (var y = users.length - 1; y >= 0; y--) {
-    output = users[y];
-    console.log(output);
-    return output;
+function getUsers() {
+  var output = "";
+  users.forEach((element) => {
+    output +=
+      element.id +
+      "-" +
+      element.firstName +
+      " " +
+      element.lastName +
+      "  is " +
+      element.age +
+      ", " +
+      element.gender +
+      "\n";
+  });
+  console.log(output);
+  return output;
 }
 //getUsers function - list of users ends here
 
 //findUserById(id) function starts here
-function findUserById(id){
-    try {
-        //add appropriate code here
-
-    } catch(error){
-        return //Change this line
-        console.log(); //Change this line
-    }   
+function findUserById(id) {
+  try {
+    //add appropriate code here
+    var user = _.find(users, function (val, key) {
+      if (val.id === id) {
+        return true;
+      }
+      return false;
+    });
+    var iFindUser =
+      user.id +
+      "-" +
+      user.firstName +
+      " " +
+      user.lastName +
+      " is " +
+      user.age +
+      ", " +
+      user.gender;
+    console.log(iFindUser);
+  } catch (error) {
+    error.message = "Cannot read property 'id'";
+    console.log(error.message);
+    return error.message; //Change this line
+  }
 }
 //findUserById(id) function ends here
 //Only change code above this line
 getUsers();
-findUserById(); //Change this line
+findUserById(1); //Change this line
 
 module.exports = findUserById;
-
