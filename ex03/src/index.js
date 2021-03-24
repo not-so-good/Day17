5f2282104bdbb69deff0f101423fbc099975730c
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-24 20:10:18 +0100

    ex03 Destructing assignment to assign variables

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+const MY_CAR = {
+  wheels: 4,
+  cylinders: 4,
+  size: 2.2,
+};
+
+function main(myCar) {
+  //Only change code below this line
+  const { cylinders: carCylinders, size: carSize } = MY_CAR;
+  //const carCylinders = MY_CAR.cylinders;
+  //const carSize = MY_CAR.size;
+  return { carCylinders, carSize };
+  //Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
