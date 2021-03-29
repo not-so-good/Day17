fc167d3a0525bd4e919df8789ddf47d69cae9d08
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-29 09:28:04 +0200

    ex06 The Collection of Rest Elements

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,7 +2,7 @@ let foo = 12, boo = 45;
 
 function main(foo, boo){
   //Only change code below this line
- [boo, foo] = [foo, boo] ;
+  [foo, boo] = [boo, foo];
   //Only change code above this line
   return {foo, boo};
 }
@@ -10,3 +10,4 @@ function main(foo, boo){
 
 console.log(main(foo, boo));
 module.exports = main;
+
