ac365d5de7aca107edafb6e47785e24bef0e81a0
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-29 09:20:19 +0200

    ex04 Destructuring Values from Nested Objects

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,12 +1,12 @@
 const BAKING_TEMPERATURES = {
     cake: {low: 180, high: 250},
-    bread: {low: 150, high: 250},
+    bread: {low: 150, high: 220},
     pie: {low: 200, high: 230}
 };
 
 function main(baking){
   //Only change code below this line
-  const {bread: {low: lowBread, high: highBread}} = BAKING_TEMPERATURES;
+  const {bread: {low: lowBread, high: highBread}} = baking;
   //const lowBread = BAKING_TEMPERATURES.bread.low;
   //const highBread = BAKING_TEMPERATURES.bread.high;
   return {lowBread, highBread};
@@ -15,3 +15,5 @@ function main(baking){
 
 console.log(main(BAKING_TEMPERATURES));
 module.exports = main;
+
+
